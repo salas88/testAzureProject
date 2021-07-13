d04ef8133b01db252e3ad83c2497409d38f18aeb
salas <vladyslav.cartel88@gmail.com>
2021-07-14 01:03:50 +0300

    add

---
+++ src/main/java/com/example/adsa/AdsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.adsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adsa/AdsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
